21ccc3c15763ea3667a69479b92f10cfa303e84d
Nishant Shishu <ns3683@columbia.edu>
2024-11-08 16:18:56 -0500

    edited with input output

--- Lab5/testbench.m
+++ Lab5/testbench.m
@@ -5,7 +5,7 @@ input = int16(randi([-32768, 32767], 1, 100));  % 100 random 16-bit integers
 coefficients = int16(rand(1, 64) * 32767);  % 64 random coefficients (16-bit integers)
 
 % Apply the FIR filter
-output = FIR_filter(input, coefficients);
+output = FIR_Filter(input, coefficients);
 
 % Save input and output to .mat files
 save('input.mat', 'input');
