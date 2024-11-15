9b8d59689bcd119ea95b8baa0acce5dd93e03dc8
cc4919 <cc4919@cadpc19.ee.columbia.edu>
2024-11-15 15:40:38 -0500

    FIFO finished

--- Lab6/FIFO.v
+++ Lab6/FIFO.v
@@ -1,14 +1,23 @@
 module FIFO#(parameter DEPTH=64, DWIDTH=16)
 (
-	input			clk, 
-				wr_en, 
-				rd_en, 
-				rst;
-	input [DWIDTH-1:0]	din;
-	output [DWIDTH-1:0]	dout;
+	input			clk,		// clock	
+				wr_en, 		// write enable
+				rd_en, 		// read enable
+				rst,		// high reset
+	input [DWIDTH-1:0]	din,		// data written into fifo
+	output reg [DWIDTH-1:0]	dout,		// data read from fifo
+	output			full,		// full signal
+				empty		// empty signal
 );
+	localparam PTR_WIDTH = $clog2(DEPTH);
+
+	reg [PTR_WIDTH-1:0] wptr = 0;
+	reg [PTR_WIDTH-1:0] rptr = 0;
+	reg [DWIDTH-1:0] fifo [0:DEPTH-1];
+
+	// write
 	always @ (posedge clk) begin
-		if(!rst) begin
+		if(rst) begin
 			wptr <= 0;
 		end else begin
 			if (wr_en & !full) begin
@@ -18,6 +27,23 @@ module FIFO#(parameter DEPTH=64, DWIDTH=16)
 		end
 	end
 
-	initial begin
+	// read
+	always @ (posedge clk) begin
+		if(rst) begin
+			rptr <= 0;
+			dout <= 0;
+		end else begin
+			if (rd_en && !empty) begin
+				dout <= fifo[rptr[PTR_WIDTH-1:0]];
+				rptr <= rptr + 1;
+			end
+		end
+	end
+
+	// full and empty
+	assign full = (wptr[PTR_WIDTH-1:0] == rptr[PTR_WIDTH-1:0]) && (wptr[PTR_WIDTH] != rptr[PTR_WIDTH]);
+	assign empty = (wtpr == rptr);
+
+endmodule
 
 
