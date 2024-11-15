c3aa241576606e8d1788047ae28e095ed02bace3
cc4919 <cc4919@cadpc19.ee.columbia.edu>
2024-11-15 16:20:42 -0500

    converter

--- Lab6/converter.v
+++ Lab6/converter.v
@@ -1,7 +1,30 @@
 module converter(
-	input wire [31:0] fixed_in,
-	output wire [15:0] float_out
+	input 			clk,
+	input wire [15:0] 	fixed_in,
+	output wire [15:0] 	float_out
 );
+	reg [4:0] i = 5'd15;
+	reg [15:0] store;
+	reg [9:0] mantissa;
+	reg [4:0] exponent;
 
-	assign float_out = fixed_in[31:16];
+	always @ (posedge clk) begin
+		if(operand != 16'd0) begin
+			i = 5'd15;
+			while (i > 0 && operand[i] == 1'b0) begin
+				i = i - 1'b1;
+			end
+			store = i;
+			exponent = store + 5'd15;
+			if (store >= 10) begin
+				mantissa = operand >> (store - 10);
+			end else begin
+				mantissa = operand >> (store - 10);
+			end
+
+			result <= {1'b0, exponent, mantissa[9,0]};
+		end else begin
+			result <= 16'd0;
+		end
+	end
 endmodule
